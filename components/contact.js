3c254192cf77ceda618c0d75c34a12cd2fa5bb26
GonzalokHM <gonzalohm93@gmail.com>
2023-08-09 20:56:27 +0200

    change wrong icon for url

--- components/contact.js
+++ components/contact.js
@@ -1,5 +1,5 @@
 const contactTemplate = `
-<div><img src="assets/WhatsApp-logo.png" alt="WhatsApp Logo" width="24" height="24" style="vertical-align: middle;">
+<div><img src="https://proyectopurpura.com/wp-content/uploads/2022/01/icono-whatsapp.png" alt="WhatsApp Logo" width="24" height="24" style="vertical-align: middle;">
 <span style="vertical-align: middle;">656947318</span>
 </div>
 <div><p>✉️ contacto@greenshop.com</p></div>
